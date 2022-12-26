9a0f2dce512fff978b249d7ae2c0498c705d8278
Nazaruk-D <nazaruk-dima@mail.ru>
2022-12-26 12:37:53 +0300

    fix: instance settings

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    // baseURL: 'http://localhost:7542/',
-    baseURL: 'https://letter-mbxudci3f-nazaruk-d.vercel.app/',
+    baseURL: 'https://backend-enshknqmq-nazaruk-d.vercel.app/',
     withCredentials: true,
-    headers: {
-        "access-control-allow-origin": "https://nazaruk-d.github.io"
-    }
 })
