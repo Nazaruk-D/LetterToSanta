73613429d5962bfb5f69537a748be3328987ee4b
Nazaruk-D <nazaruk-dima@mail.ru>
2023-02-26 14:47:56 +0300

    baseURL

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'https://backend-enshknqmq-nazaruk-d.vercel.app/',
+    baseURL: 'https://backend-k3moe0viz-nazaruk-d.vercel.app/',
+    // baseURL: 'http://localhost:7542/',
     withCredentials: true,
 })
